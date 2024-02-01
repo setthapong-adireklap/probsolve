3d535f2b5ebd046ee747150a8a2f192c6d584b24
ppich <79843370+setthapong-adireklap@users.noreply.github.com>
2024-02-01 18:34:58 +0700

    Update likemax2.cpp

--- probSolve/Lab03/likemax2.cpp
+++ probSolve/Lab03/likemax2.cpp
@@ -1,17 +1,30 @@
 #include <iostream>
-#include <map>
-#include <queue>
+#include <unordered_map>
+#include <set>
 
 using namespace std;
 
 int main(){
 
     int n, mode, data;
-    map<int,int> like;
+    unordered_map<int,int> like;
+    unordered_map<int,int> reverseCurr;
+    unordered_map<int,int> curr;
+    set<pair<int,int>> set_like;
+    
     cin >> n;
 
     for(int i = 0 ; i<n ; i++){
         cin >> mode >> data;
+
+        auto it = set_like.find(make_pair(like[data],curr[data]));
+        if(it != set_like.end()){
+            set_like.erase(it);
+        }
+
+        reverseCurr[i] = data;
+        curr[data] = i;
+
         switch(mode){
         case 0:
             like[data] -= 1;
@@ -21,6 +34,12 @@ int main(){
             like[data] += 1;
             break;
         }
-    }
+        set_like.insert(make_pair(like[data],i));
 
+        auto res = set_like.end();
+        res--;
+        
+        cout << reverseCurr[res->second] << endl;
+    }
+    return 0;
 }
